303c35809135ba41cf41138950ae5fac46d5c010
marcelogdiaz <marcelogdiaz@gmail.com>
2023-05-28 21:36:40 -0300

    Commit Inicial

---
+++ WindowsFormsAppEjemplo/Formularios/Form1.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace WindowsFormsAppEjemplo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBoxMostrarCombo = new System.Windows.Forms.CheckBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxTitulo = new System.Windows.Forms.TextBox();
+            this.panelPrincipal = new System.Windows.Forms.Panel();
+            this.labelEvento = new System.Windows.Forms.Label();
+            this.buttonGroupBox = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.apellido = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dni = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.panelPrincipal.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(482, 8);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Mostrar Titulo";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBoxMostrarCombo
+            // 
+            this.checkBoxMostrarCombo.AutoSize = true;
+            this.checkBoxMostrarCombo.Location = new System.Drawing.Point(21, 29);
+            this.checkBoxMostrarCombo.Name = "checkBoxMostrarCombo";
+            this.checkBoxMostrarCombo.Size = new System.Drawing.Size(97, 17);
+            this.checkBoxMostrarCombo.TabIndex = 1;
+            this.checkBoxMostrarCombo.Text = "Mostrar Combo";
+            this.checkBoxMostrarCombo.UseVisualStyleBackColor = true;
+            this.checkBoxMostrarCombo.CheckedChanged += new System.EventHandler(this.checkBoxMostrarCombo_CheckedChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Opcion1",
+            "Opcion2",
+            "Opcion3"});
+            this.comboBox1.Location = new System.Drawing.Point(139, 29);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(146, 21);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.Text = "-Seleccione Una Opcion-";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(258, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Ingrese el titulo del FORMULARIO:";
+            this.label1.Leave += new System.EventHandler(this.label1_Leave);
+            // 
+            // textBoxTitulo
+            // 
+            this.textBoxTitulo.Location = new System.Drawing.Point(276, 14);
+            this.textBoxTitulo.Name = "textBoxTitulo";
+            this.textBoxTitulo.Size = new System.Drawing.Size(190, 26);
+            this.textBoxTitulo.TabIndex = 4;
+            // 
+            // panelPrincipal
+            // 
+            this.panelPrincipal.AutoSize = true;
+            this.panelPrincipal.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.panelPrincipal.Controls.Add(this.labelEvento);
+            this.panelPrincipal.Controls.Add(this.buttonGroupBox);
+            this.panelPrincipal.Controls.Add(this.button1);
+            this.panelPrincipal.Controls.Add(this.textBoxTitulo);
+            this.panelPrincipal.Controls.Add(this.label1);
+            this.panelPrincipal.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panelPrincipal.Location = new System.Drawing.Point(12, 12);
+            this.panelPrincipal.Name = "panelPrincipal";
+            this.panelPrincipal.Size = new System.Drawing.Size(602, 89);
+            this.panelPrincipal.TabIndex = 5;
+            // 
+            // labelEvento
+            // 
+            this.labelEvento.AutoSize = true;
+            this.labelEvento.Location = new System.Drawing.Point(17, 53);
+            this.labelEvento.Name = "labelEvento";
+            this.labelEvento.Size = new System.Drawing.Size(168, 20);
+            this.labelEvento.TabIndex = 6;
+            this.labelEvento.Text = "Evento Boton Habilitar";
+            // 
+            // buttonGroupBox
+            // 
+            this.buttonGroupBox.Location = new System.Drawing.Point(482, 44);
+            this.buttonGroupBox.Name = "buttonGroupBox";
+            this.buttonGroupBox.Size = new System.Drawing.Size(102, 29);
+            this.buttonGroupBox.TabIndex = 5;
+            this.buttonGroupBox.Text = "Habilitar";
+            this.buttonGroupBox.UseVisualStyleBackColor = true;
+            this.buttonGroupBox.Click += new System.EventHandler(this.buttonGroupBox_Click);
+            this.buttonGroupBox.MouseClick += new System.Windows.Forms.MouseEventHandler(this.buttonGroupBox_MouseClick);
+            this.buttonGroupBox.MouseUp += new System.Windows.Forms.MouseEventHandler(this.buttonGroupBox_MouseUp);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.AutoSize = true;
+            this.groupBox1.Controls.Add(this.checkBoxMostrarCombo);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Enabled = false;
+            this.groupBox1.Location = new System.Drawing.Point(12, 121);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(400, 69);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Informacion General";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.nombre,
+            this.apellido,
+            this.dni});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(342, 143);
+            this.dataGridView1.TabIndex = 7;
+            // 
+            // nombre
+            // 
+            this.nombre.HeaderText = "Nombre";
+            this.nombre.Name = "nombre";
+            this.nombre.ReadOnly = true;
+            // 
+            // apellido
+            // 
+            this.apellido.HeaderText = "Apellido";
+            this.apellido.Name = "apellido";
+            this.apellido.ReadOnly = true;
+            // 
+            // dni
+            // 
+            this.dni.HeaderText = "DNI";
+            this.dni.Name = "dni";
+            this.dni.ReadOnly = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.dataGridView1);
+            this.panel1.Location = new System.Drawing.Point(12, 206);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(368, 303);
+            this.panel1.TabIndex = 8;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(424, 206);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(287, 296);
+            this.tabControl1.TabIndex = 9;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(279, 270);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(279, 270);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(733, 549);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panelPrincipal);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Formulario de Ejemplo";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panelPrincipal.ResumeLayout(false);
+            this.panelPrincipal.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBoxMostrarCombo;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBoxTitulo;
+        private System.Windows.Forms.Panel panelPrincipal;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button buttonGroupBox;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn apellido;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dni;
+        private System.Windows.Forms.Label labelEvento;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+    }
+}
+
---
+++ WindowsFormsAppEjemplo/Formularios/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppEjemplo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = "Formulario cargado...";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Text = textBoxTitulo.Text;
+        }
+
+        private void checkBoxMostrarCombo_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBoxMostrarCombo.Checked)
+            {
+                comboBox1.Visible = true;
+            }
+            else {
+                comboBox1.Visible = false;
+            }
+        }
+
+        private void buttonGroupBox_Click(object sender, EventArgs e)
+        {
+            groupBox1.Enabled = true;
+            labelEvento.Text = "Click";   
+        }
+
+        private void label1_Leave(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonGroupBox_MouseClick(object sender, MouseEventArgs e)
+        {
+            labelEvento.Text = "MouseClick";
+        }
+
+        private void buttonGroupBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            labelEvento.Text = "MouseDown";
+        }
+
+        private void buttonGroupBox_MouseUp(object sender, MouseEventArgs e)
+        {
+            labelEvento.Text = "MouseUp";
+        }
+    }
+}
